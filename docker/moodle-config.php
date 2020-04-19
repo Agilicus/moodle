de61683aae4076cca6d4bb9d44c91ae365a5e4fd
Don Bowman <don@agilicus.com>
2020-04-19 13:11:28 -0400

    fix: incorrect spelling on CfG
    docker/moodle-config.php:571 had bad syntax for lock setup

--- docker/moodle-config.php
+++ docker/moodle-config.php
@@ -568,7 +568,7 @@ if ( getenv('SSL_PROXY') == "true" ) {
 //      Uses lock files stored by default in the dataroot. Whether this
 //      works on clusters depends on the file system used for the dataroot.
 //
-$CfG->lock_factory = "\\core\\lock\\db_record_lock_factory" ; # - DB locking based on table rows.
+$CFG->lock_factory = "\\core\\lock\\db_record_lock_factory" ; # - DB locking based on table rows.
 //
 // "\\core\\lock\\postgres_lock_factory" - DB locking based on postgres advisory locks.
 //
