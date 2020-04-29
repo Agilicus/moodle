8fe93dffc26a83099cdf9150df2fc54d31b58a26
Jamie Chapman-Brown <jamie.chapman-brown@agilicus.com>
2020-04-29 15:59:32 -0400

    fix: always editing the current hosting company

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $PAGE->set_cacheable(false);
 require_course_login($SITE);
 
 // IOMAD - Set the theme if the server hostname matches one of ours.
-if ($company = $DB->get_record('company', array('hostname' => $_SERVER["SERVER_NAME"]))) {
+if ($company = $DB->get_record('company', array('hostname' => $_SERVER['HTTP_HOST']))) {
     $hascompanybyurl = true;
     // set the current editing company to be this.
     $SESSION->currenteditingcompany = $company->id;
