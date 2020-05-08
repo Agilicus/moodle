381d27b212cca4d9854ef601bb22ecc56ec4e4e2
Jamie Chapman-Brown <jamie.chapman-brown@agilicus.com>
2020-05-08 17:54:51 -0400

    fix: now cleans all dangling cached files (css and js), from the three tables: mdl_tool_objectfs_objects, mdl_hvp_libraries_cachedassets, mdl_files

--- admin/cli/clean_hvp_cache.php
+++ admin/cli/clean_hvp_cache.php
@@ -4,14 +4,17 @@ define('CLI_SCRIPT', true);
 
 require(__DIR__.'/../../config.php');
 require_once($CFG->libdir.'/clilib.php');
-require_once(__DIR__ . '/../tool/objectfs/lib.php');
+
 
 $help =
-"clean the hvp cache of any files that aren't in storage or remote.";
+"clean the hvp cache of any files that aren't in storage or remote.
+
+Options:
+--run                       Execute clean. If this option is not set, then the script will be run in a dry mode.";
 
 // Now get cli options.
 list($options, $unrecognized) = cli_get_params(
-    array(),
+    array('run' => false, 'help' => false),
     array('h'=>'help')
 );
 
@@ -20,21 +23,104 @@ if ($options['help']) {
     die;
 }
 
-echo "hvp cache\n";
-$fields = 'distinct h.id';
-$from = "{files} f LEFT JOIN {tool_objectfs_objects} o on f.contenthash = o.contenthash LEFT JOIN {hvp_libraries_cachedassets} h on h.hash = regexp_replace(f.filename, '\.(.*)', '')";
-$where = 'o.location = ?';
-$params = [OBJECT_LOCATION_ERROR];
 
-$sql = "SELECT 
-{$fields}
-FROM {$from}
-WHERE {$where}";
+function get_drop_query_from_idlist($array) {
+    return 'id in (' . implode(',', $array) . ')';
+}
 
-$rawdata = $DB->get_records_sql($sql, $params);
+function get_drop_query($query_result, $category) {
+    $to_drop = array();
 
-echo 'drop missing cache files\n';
-foreach($rawdata as $entry) {
-    echo 'drop' . $entry->id . "\n";
-    $DB->delete_records('hvp_libraries_cachedassets', array('id'=>$entry->id));
+    foreach($query_result as $hvp_entry) {
+        array_push($to_drop, $hvp_entry->$category);
+    }
+
+    // this removes duplicate entries
+    $to_drop = array_combine($to_drop, $to_drop);
+    return get_drop_query_from_idlist($to_drop);
+}
+
+// for entries with more than one id in them
+function get_drop_query_list($query_result, $category) {
+    $to_drop = array();
+
+    foreach($query_result as $hvp_entry) {
+        $list = trim($hvp_entry->$category, "{}");
+        $toadd =  explode(',', $list);
+        foreach($toadd as $add) {
+            array_push($to_drop, $add);
+        }
+    }
+
+    $to_drop = array_combine($to_drop, $to_drop);
+    return 'id in (' . implode(',', $to_drop) . ')';
+}
+
+try {
+
+
+    $transaction = $DB->start_delegated_transaction();
+
+    echo "hvp cache\n";
+    $fields = 'o.id as objectfsids,
+               f.id as fileid,
+               f.contenthash as contenthash,
+               f.pathnamehash as pathnamehash, 
+               f.filename as filename,
+               array_agg(h.id) as hvp_cache_ids' ;
+    $from = "{files} f RIGHT JOIN {tool_objectfs_objects} o ON f.contenthash = o.contenthash RIGHT JOIN {hvp_libraries_cachedassets} h on h.hash = regexp_replace(f.filename, '\.(.*)', '')";
+    $where = 'o.location = ? GROUP BY f.id, o.id, f.contenthash, f.pathnamehash, f.filename';
+    $params = ['-1'];
+
+    $sql = "SELECT 
+    {$fields}
+    FROM {$from}
+    WHERE {$where}";
+
+    $rawdata = $DB->get_records_sql($sql, $params);
+
+    if (!$rawdata) {
+        echo "nothing to clean!";
+        exit;
+    }
+
+    if( $options['run'] ){
+        echo "dropping the following from mdl_files and mdl_h5p_libraries_cachedassets:\n";
+    }
+    else {
+        echo "would drop the following:\n";
+    }
+
+    foreach($rawdata as $entry) {
+    
+        echo print_r($entry, true);
+    }
+
+    if( !$options['run'] ){
+        $transaction->allow_commit();
+    } else {
+
+        echo "dropping from mdl_files\n";
+        $file_delete_query = get_drop_query($rawdata, 'fileid');
+        echo $file_delete_query . "\n";
+        $DB->delete_records_select('files', $file_delete_query);
+
+        echo "dropping from mdl_h5p_libraries_cachedassets\n";
+        $hvp_delete_query = get_drop_query_list($rawdata, 'hvp_cache_ids');
+        echo $hvp_delete_query . "\n";
+        $DB->delete_records_select('h5p_libraries_cachedassets', $hvp_delete_query);
+
+        echo "dropping from mdl_tool_objectfs_objects\n";
+        $object_delete_query = get_drop_query($rawdata, 'objectfsids');
+        echo $object_delete_query . "\n";
+        $DB->delete_records_select('hvp_libraries_cachedassets', $object_delete_query);
+
+
+        $transaction->allow_commit();
+    }
+
+
+} catch (Exception $e) {
+    //extra cleanup steps
+    $transaction->rollback($e); // rethrows exception
 }
